ca6eee321ab867f50c5d85f1647b30030e5eb0ae
anttikekki <antti.kekki@gmail.com>
2015-04-20 21:30:45 +0300

    Import work table ready to final

--- dataImport/MuinaismuistotImport.php
+++ dataImport/MuinaismuistotImport.php
@@ -20,26 +20,55 @@ class MuinaismuistotImport {
 	}
 
 	public function start() {
-		echo "Starting import<br>";
-		$this->dropAndCreateWorkTable();
-		$this->importCSV();
+		$this->printMessage("Starting import");
+
+		$this->dropAndCreateMuinaismuistopisteWorkTable();
+		$this->importCSVToMuinaismuistopisteWork();
+
+		//Ajoitus
 		$this->createAjoituTable();
 		$this->insertDataToAjoitus();
 		$this->createMuinaisjaannospisteAjoitusTable();
 		$this->insertDataToMuinaisjaannospisteAjoitus();
-		echo "Import finished<br>";
+
+		//Tyyppi
+		$this->createAndPopoulateTyyppiTable();
+		$this->updateTyyppiToID();
+
+		//Alatyyppi
+		$this->createAndPopoulateAlatyyppiTable();
+		$this->updateAlatyyppiToID();
+
+		//Laji
+		$this->createAndPopoulateLajiTable();
+		$this->updateLajiToID();
+
+		//Kunta
+		$this->createAndPopoulateKuntaTable();
+		$this->updateKuntaToID();
+
+		//Tuhoutunut and vedenalainen
+		$this->updateTuhoutunutToBoolean();
+		$this->updateVedenalainenToBoolean();
+
+		$this->printMessage("Import finished");
+	}
+
+	protected function printMessage($message) {
+		echo $message."<br>";
+		flush();
 	}
 
 	protected function checkErrorAndDieIfPresent() {
 		if($this->database->error()[0] != '0000') {
-			echo "Last SQL Query: ".$this->database->last_query()."<br>";
-			echo "Error: ";
+			$this->printMessage("Last SQL Query: ".$this->database->last_query());
+			$this->printMessage("Error: ");
 			var_dump($this->database->error());
 			die();
 		}
 	}
 
-	protected function dropAndCreateWorkTable() {
+	protected function dropAndCreateMuinaismuistopisteWorkTable() {
 		$createTableSql = "
 			DROP TABLE IF EXISTS `muinaisjaannospisteet_work`;
 			CREATE TABLE IF NOT EXISTS `muinaisjaannospisteet_work` (
@@ -59,8 +88,8 @@ class MuinaismuistotImport {
 			  `PAIKANNU0` int(1) NOT NULL,
 			  `SELITE` text NOT NULL,
 			  `TUHOUTUNUT` varchar(5) NOT NULL,
-			  `LUONTIPVM` varchar(10) NOT NULL,
-			  `MUUTOSPVM` varchar(10) NOT NULL,
+			  `LUONTIPVM` date,
+			  `MUUTOSPVM` date,
 			  `ZALA` decimal(20,12) NOT NULL,
 			  `ZYLA` decimal(20,12) NOT NULL,
 			  `VEDENALAIN` varchar(1) NOT NULL,
@@ -70,11 +99,43 @@ class MuinaismuistotImport {
 		$this->database->query($createTableSql);
 		$this->checkErrorAndDieIfPresent();
 
-		echo "Created muinaisjaannospisteet_work table<br>";
+		$this->printMessage("Created muinaisjaannospisteet_work table");
 	}
 
-	protected function importCSV() {
-		echo "Starting import to muinaisjaannospisteet_work table<br>";
+	protected function dropAndCreateMuinaismuistopisteFinalTable() {
+		$createTableSql = "
+			DROP TABLE IF EXISTS `muinaisjaannospisteet_work`;
+			CREATE TABLE IF NOT EXISTS `muinaisjaannospisteet_work` (
+			  `ID` int(8) NOT NULL AUTO_INCREMENT,
+			  `X` decimal(20,12) NOT NULL,
+			  `Y` decimal(20,12) NOT NULL,
+			  `KUNTA` int(3) NOT NULL,
+			  `MJTUNNUS` int(8) NOT NULL,
+			  `KOHDENIMI` varchar(256) NOT NULL,
+			  `AJOITUS` int(3) NOT NULL,
+			  `TYYPPI` int(3) NOT NULL,
+			  `ALATYYPPI` int(3) NOT NULL,
+			  `LAJI` int(3) NOT NULL,
+			  `PAIKANNUST` int(5) NOT NULL,
+			  `PAIKANNU0` int(1) NOT NULL,
+			  `SELITE` text NOT NULL,
+			  `TUHOUTUNUT` tinyint(1) NOT NULL,
+			  `LUONTIPVM` date,
+			  `MUUTOSPVM` date,
+			  `ZALA` decimal(20,12) NOT NULL,
+			  `ZYLA` decimal(20,12) NOT NULL,
+			  `VEDENALAIN` tinyint(1) NOT NULL,
+			  PRIMARY KEY (ID)
+			) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+		";
+		$this->database->query($createTableSql);
+		$this->checkErrorAndDieIfPresent();
+
+		$this->printMessage("Created muinaisjaannospisteet_work table");
+	}
+
+	protected function importCSVToMuinaismuistopisteWork() {
+		$this->printMessage("Starting import to muinaisjaannospisteet_work table");
 
 		/*
 		* When turned on, PHP will examine the data read by fgets() and file() to see if it is using Unix, MS-Dos or Macintosh line-ending conventions. 
@@ -119,16 +180,30 @@ class MuinaismuistotImport {
 				'PAIKANNU0' => (int)$data[12],
 				'SELITE' => $data[13],
 				'TUHOUTUNUT' => $data[14],
-				'LUONTIPVM' => $data[15],
-				'MUUTOSPVM' => $data[16],
+				'LUONTIPVM' => $this->parseDateToMySql($data[15]),
+				'MUUTOSPVM' => $this->parseDateToMySql($data[16]),
 				'ZALA' => (double)$data[17],
 				'ZYLA' => (double)$data[18],
 				'VEDENALAIN' => $data[19]
 			]);
 
 			$this->checkErrorAndDieIfPresent();
+
+			if($rowIndex % 5000 == 0) {
+				$this->printMessage($rowIndex . " rows imported");
+			}
 		}
-		echo $rowIndex . " rows imported<br>";
+
+		fclose($handle);
+		$this->printMessage($rowIndex . " rows imported");
+	}
+
+	protected function parseDateToMySql($dateString) {
+		$date = date_create_from_format('Y/m/d', $dateString);
+		if($date === false) {
+			return null;
+		}
+		return $date->format('Y-m-d');
 	}
 
 	protected function createAjoituTable() {
@@ -143,7 +218,7 @@ class MuinaismuistotImport {
 		$this->database->query($createTableSql);
 		$this->checkErrorAndDieIfPresent();
 
-		echo "Created ajoitus table<br>";
+		$this->printMessage("Created ajoitus table");
 	}
 
 	protected function insertDataToAjoitus() {
@@ -170,7 +245,7 @@ class MuinaismuistotImport {
 			$this->checkErrorAndDieIfPresent();
 		}
 
-		echo "Inserted distinct data to ajoitus table<br>";
+		$this->printMessage("Inserted distinct data to ajoitus table");
 	}
 
 	protected function parseAjoituksetStringToArray($str) {
@@ -190,7 +265,7 @@ class MuinaismuistotImport {
 		$this->database->query($createTableSql);
 		$this->checkErrorAndDieIfPresent();
 
-		echo "Created muinaisjaannospiste_ajoitus table<br>";
+		$this->printMessage("Created muinaisjaannospiste_ajoitus table");
 	}
 
 	protected function getAjoituksetIdByNameMap() {
@@ -229,7 +304,207 @@ class MuinaismuistotImport {
 			}
 		}
 
-		echo "Inserted link data to muinaisjaannospiste_ajoitus table<br>";
+		$this->printMessage("Inserted link data to muinaisjaannospiste_ajoitus table");
+	}
+
+	protected function createAndPopoulateTyyppiTable() {
+		$createTableSql = "
+			DROP TABLE IF EXISTS `tyyppi`;
+			CREATE TABLE IF NOT EXISTS `tyyppi` (
+			  `ID` int(2) NOT NULL AUTO_INCREMENT,
+			  `NIMI` varchar(50) NOT NULL,
+			  PRIMARY KEY (ID)
+			) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+		";
+		$this->database->query($createTableSql);
+		$this->checkErrorAndDieIfPresent();
+
+		$sql = "
+			INSERT INTO tyyppi(NIMI)
+			SELECT DISTINCT TYYPPI
+			FROM muinaisjaannospisteet_work
+		";
+		$this->database->query($sql);
+		$this->checkErrorAndDieIfPresent();
+
+		$this->printMessage("Created and populated tyyppi table");
+	}
+
+	protected function updateTyyppiToID() {
+		$sql = "
+			SELECT ID, NIMI
+			FROM tyyppi
+		";
+		$tyypit = $this->database->query($sql)->fetchAll();
+		$this->checkErrorAndDieIfPresent();
+
+		foreach ($tyypit as $tyyppi) {
+			$this->database->update("muinaisjaannospisteet_work", [
+				"TYYPPI" => $tyyppi["ID"]
+			], [
+				"TYYPPI" => $tyyppi["NIMI"]
+			]);
+		}
+
+		$this->printMessage("Replaced tyyppi name with id");
+	}
+
+	protected function createAndPopoulateAlatyyppiTable() {
+		$createTableSql = "
+			DROP TABLE IF EXISTS `alatyyppi`;
+			CREATE TABLE IF NOT EXISTS `alatyyppi` (
+			  `ID` int(2) NOT NULL AUTO_INCREMENT,
+			  `NIMI` varchar(50) NOT NULL,
+			  PRIMARY KEY (ID)
+			) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+		";
+		$this->database->query($createTableSql);
+		$this->checkErrorAndDieIfPresent();
+
+		$sql = "
+			INSERT INTO alatyyppi(NIMI)
+			SELECT DISTINCT ALATYYPPI
+			FROM muinaisjaannospisteet_work
+		";
+		$this->database->query($sql);
+		$this->checkErrorAndDieIfPresent();
+
+		$this->printMessage("Created and populated alatyyppi table");
+	}
+
+	protected function updateAlatyyppiToID() {
+		$sql = "
+			SELECT ID, NIMI
+			FROM alatyyppi
+		";
+		$rows = $this->database->query($sql)->fetchAll();
+		$this->checkErrorAndDieIfPresent();
+
+		foreach ($rows as $row) {
+			$this->database->update("muinaisjaannospisteet_work", [
+				"ALATYYPPI" => $row["ID"]
+			], [
+				"ALATYYPPI" => $row["NIMI"]
+			]);
+		}
+
+		$this->printMessage("Replaced alatyyppi name with id");
+	}
+
+	protected function createAndPopoulateLajiTable() {
+		$createTableSql = "
+			DROP TABLE IF EXISTS `laji`;
+			CREATE TABLE IF NOT EXISTS `laji` (
+			  `ID` int(2) NOT NULL AUTO_INCREMENT,
+			  `NIMI` varchar(50) NOT NULL,
+			  PRIMARY KEY (ID)
+			) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+		";
+		$this->database->query($createTableSql);
+		$this->checkErrorAndDieIfPresent();
+
+		$sql = "
+			INSERT INTO laji(NIMI)
+			SELECT DISTINCT LAJI
+			FROM muinaisjaannospisteet_work
+		";
+		$this->database->query($sql);
+		$this->checkErrorAndDieIfPresent();
+
+		$this->printMessage("Created and populated laji table");
+	}
+
+	protected function updateLajiToID() {
+		$sql = "
+			SELECT ID, NIMI
+			FROM laji
+		";
+		$rows = $this->database->query($sql)->fetchAll();
+		$this->checkErrorAndDieIfPresent();
+
+		foreach ($rows as $row) {
+			$this->database->update("muinaisjaannospisteet_work", [
+				"LAJI" => $row["ID"]
+			], [
+				"LAJI" => $row["NIMI"]
+			]);
+		}
+
+		$this->printMessage("Replaced laji name with id");
+	}
+
+	protected function createAndPopoulateKuntaTable() {
+		$createTableSql = "
+			DROP TABLE IF EXISTS `kunta`;
+			CREATE TABLE IF NOT EXISTS `kunta` (
+			  `ID` int(2) NOT NULL AUTO_INCREMENT,
+			  `NIMI` varchar(50) NOT NULL,
+			  PRIMARY KEY (ID)
+			) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+		";
+		$this->database->query($createTableSql);
+		$this->checkErrorAndDieIfPresent();
+
+		$sql = "
+			INSERT INTO kunta(NIMI)
+			SELECT DISTINCT KUNTA
+			FROM muinaisjaannospisteet_work
+		";
+		$this->database->query($sql);
+		$this->checkErrorAndDieIfPresent();
+
+		$this->printMessage("Created and populated kunta table");
+	}
+
+	protected function updateKuntaToID() {
+		$sql = "
+			SELECT ID, NIMI
+			FROM kunta
+		";
+		$rows = $this->database->query($sql)->fetchAll();
+		$this->checkErrorAndDieIfPresent();
+
+		foreach ($rows as $row) {
+			$this->database->update("muinaisjaannospisteet_work", [
+				"KUNTA" => $row["ID"]
+			], [
+				"KUNTA" => $row["NIMI"]
+			]);
+		}
+
+		$this->printMessage("Replaced kunta name with id");
+	}
+
+	protected function updateTuhoutunutToBoolean() {
+		$this->database->update("muinaisjaannospisteet_work", [
+			"TUHOUTUNUT" => 1
+		], [
+			"TUHOUTUNUT" => "Kyllä"
+		]);
+
+		$this->database->update("muinaisjaannospisteet_work", [
+			"TUHOUTUNUT" => 0
+		], [
+			"TUHOUTUNUT" => "Ei"
+		]);
+
+		$this->printMessage("Replaced tuhoutunut name with boolean");
+	}
+
+	protected function updateVedenalainenToBoolean() {
+		$this->database->update("muinaisjaannospisteet_work", [
+			"VEDENALAIN" => 1
+		], [
+			"VEDENALAIN" => "k"
+		]);
+
+		$this->database->update("muinaisjaannospisteet_work", [
+			"VEDENALAIN" => 0
+		], [
+			"VEDENALAIN" => "e"
+		]);
+
+		$this->printMessage("Replaced vedenalainen name with boolean");
 	}
 
 }
