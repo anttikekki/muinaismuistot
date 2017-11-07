c4ee04c23e022bfd662411a9c4769c8d06d3ecfd
Antti Kekki <antti.kekki@solita.fi>
2017-11-07 22:02:46 +0200

    Siirretty kuvat ja xml-tiedostot dist -> src, josta ne sitten buildissa kopioidaan dist-kansioon

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
@@ -15,22 +17,30 @@ module.exports = {
           loader: 'babel-loader'
       },
       {
-        test: /\.css$/,
-        use: [
-          {loader: 'style-loader'},
-          {loader: 'css-loader'}
-        ]
+          test: /\.css$/,
+          use: [
+            {loader: 'style-loader'},
+            {loader: 'css-loader'}
+          ]
       },
       {
-        test: /\.(woff2?|ttf|eot|svg)$/,
-        loader: 'url-loader'
+          test: /\.(woff2?|ttf|eot|svg)$/,
+          loader: 'url-loader'
       }
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(['dist'], {
+        verbose: true,
+        dry: false,
+    }),
     new HtmlWebpackPlugin({
         template:  'src/index.html',
         filename: 'index.html'
-    })
+    }),
+     new CopyWebpackPlugin([
+       { from: 'src/capabilities', to: 'capabilities' },
+       { from: 'src/images', to: 'images' }
+     ])
   ]
 };
