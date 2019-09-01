9010b01e355311b533497355af4a2305df2ed4e8
Antti Kekki <antti.kekki@reaktor.com>
2019-09-01 14:59:24 +0300

    Split info page panels to separate components

---
+++ src/ui/page/infoPage/component/SiteInfoPanel.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import { Panel } from "../../../component/Panel";
+
+export const SiteInfoPanel: React.FC = () => {
+  return (
+    <Panel title="Tietoa sivustosta">
+      <p>Tällä sivustolla näet kartalla seuraavien rekisterien tiedot:</p>
+
+      <h5>Museovirasto</h5>
+      <ul>
+        <li>
+          <a href="https://www.kyppi.fi/palveluikkuna/mjreki/read/asp/r_default.aspx">
+            Muinaisjäännösrekisteri
+          </a>
+        </li>
+        <li>
+          <a href="https://www.kyppi.fi/palveluikkuna/rapea/read/asp/r_default.aspx">
+            Rakennusperintörekisteri
+          </a>
+        </li>
+        <li>
+          <a href="https://www.museovirasto.fi/fi/tietoa-meista/kansainvalinen-toiminta/maailmanperintokohteet-suomessa">
+            Maailmanperintökohteet
+          </a>
+        </li>
+        <li>
+          <a href="http://www.rky.fi/">
+            Valtakunnallisesti merkittävät rakennetut kulttuuriympäristöt
+          </a>
+        </li>
+      </ul>
+
+      <h5>Ahvenamaan paikallishallinto</h5>
+      <ul>
+        <li>
+          <a href="http://www.regeringen.ax/kulturarv/arkeologi-fornlamningar/fornlamningsregistret">
+            Muinaisjäännösrekisteri
+          </a>
+        </li>
+      </ul>
+
+      <p>
+        Kaikki näytettävät kohteet ovat avointa dataa ja haettu rekisterin
+        ylläpitäjän tarjoamasta karttapalvelusta.
+      </p>
+
+      <p>
+        Kohteista saa lisätietoikkunan näkyviin klikkaamalla kohdetta kartalla.
+        Tästä näkymästä on myös linkki kohteen lisätietoihin rekisterin
+        ylläpitäjän sivustolla. Kohteen otsikon vieressä olevasta kuvakkeesta (
+        <span className="glyphicon glyphicon-link" aria-hidden="true" />) saa
+        talteen pysyvän linkin kohteen sijaintiin kartalla.
+      </p>
+
+      <h5>Paikannus</h5>
+      <p>
+        Kartta keskitetään oletuksena nykyiseen sijaintiisi jos annat selaimelle
+        luvan luovuttaa se tieto. Keskityksen nykyiseen sijaintiin voi suorittaa
+        myös myöhemmin{" "}
+        <span className="glyphicon glyphicon-screenshot" aria-hidden="true" />{" "}
+        painikkeesta.
+      </p>
+
+      <h5>Haku</h5>
+      <p>
+        Museoviraston rekisterien kohteita pystyy hakemaan nimen perusteella
+        hakunäkymässä (
+        <span className="glyphicon glyphicon-search" aria-hidden="true" />
+        ). Hakutuloksen klikkaaminen keskittää kartan kohteeseen ja lisää siihen
+        paikkamerkin <img src="images/map-pin-small.png" />.
+      </p>
+
+      <h5>Asetukset</h5>
+      <p>
+        Pohjakartan voi vaihtaa taustakartan ja maastokartan välillä asetuksista
+        (
+        <span className="glyphicon glyphicon-cog" aria-hidden="true" />
+        ). Samasta näkymästä voi piilottaa Museoviraston rekisterien kohteita
+        tyypin ja muinaisjäännöksiä myös ajoituksen mukaan jos haluaa
+        tarkastella esimerkiksi pelkästään muinaisjäännöksiä rautakaudelta.
+      </p>
+
+      <p>
+        Palautetta sivustosta otetaan mielellään vastaan osoitteeseen{" "}
+        <a href="mailto:antti.kekki@gmail.com">antti.kekki@gmail.com</a>
+      </p>
+    </Panel>
+  );
+};
---
+++ src/ui/page/infoPage/component/VersionHistoryPanel.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import { Panel } from "../../../component/Panel";
+
+export const VersionHistoryPanel: React.FC = () => {
+  return (
+    <Panel title="Tietoa sivustosta">
+      <h5>Elokuu 2019</h5>
+      <p>
+        Siirrytty käyttämään Ahvenanmaan maakuntahallinnon uutta{" "}
+        <a href="https://www.kartor.ax/datasets/fornminnen">kartor.ax</a>{" "}
+        karttapalvelua. Samalla kohteiden haku alkoi toimimaan myös Ahvenanmaan
+        kohteille.
+      </p>
+      <p>
+        Kartalle klikattujen kohteiden listassa näkyy nyt otsikossa kohteen
+        nimi. Listassa voi nyt myös olla monta saman tyyppistä kohdetta.
+      </p>
+
+      <br />
+
+      <h5>Heinäkuu 2019</h5>
+      <p>
+        Korjattu Museoviraston muinaisjäännösrekisterin pisteiden filtteröinti
+        ajoituksen ja tyypin perusteella.
+      </p>
+
+      <br />
+
+      <h5>Joulukuu 2018</h5>
+      <p>
+        Siirrytty käyttämään Maanmittauslaitoksen uutta{" "}
+        <a href="https://www.maanmittauslaitos.fi/kartat-ja-paikkatieto/asiantuntevalle-kayttajalle/kartta-ja-paikkatietojen-rajapintapalvelut-17">
+          karttapalvelua
+        </a>
+        , jonka myötä taustakartan ulkonäkö muuttui selkeämmäksi ja{" "}
+        <a href="https://www.maanmittauslaitos.fi/kartat-ja-paikkatieto/asiantuntevalle-kayttajalle/tuotekuvaukset/ortokuva">
+          Ortokuvat
+        </a>{" "}
+        saatiin käyttöön.
+      </p>
+      <p>
+        Siirrytty käyttämään Museoviraston uuta{" "}
+        <a href="https://www.avoindata.fi/data/fi/dataset/museoviraston-kulttuuriymparistoaineistot-suojellut-kohteet-wms-palvelu">
+          karttapalvelua
+        </a>
+        , jonka myötä saatiin käyttöön ruskeat "Muu kulttuuriperintökohde"
+        -pisteet kartalle.
+      </p>
+
+      <br />
+
+      <h5>Marraskuu 2017</h5>
+      <p>
+        Lisätty kartalle Ahvenanmaan maakuntahallinnon{" "}
+        <a href="http://www.regeringen.ax/kulturarv/arkeologi-fornlamningar/fornlamningsregistret">
+          muinaijäännösten rekisteri
+        </a>
+        .
+      </p>
+
+      <br />
+
+      <h5>Lokakuu 2015</h5>
+      <p>Ensimmäinen versio julkaistu.</p>
+    </Panel>
+  );
+};
