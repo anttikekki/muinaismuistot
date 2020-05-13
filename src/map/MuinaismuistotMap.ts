5cd59b178b2bec4260aa5953dfe11009a45f8ac0
Antti Kekki <antti.kekki@reaktor.com>
2020-05-13 19:02:26 +0300

    Disable map rotation

--- src/map/MuinaismuistotMap.ts
+++ src/map/MuinaismuistotMap.ts
@@ -69,6 +69,7 @@ export default class MuinaismuistotMap {
       center: [385249.63630000036, 6672695.7579],
       projection: "EPSG:3067",
       zoom: 8,
+      enableRotation: false,
     });
 
     this.map = new Map({
