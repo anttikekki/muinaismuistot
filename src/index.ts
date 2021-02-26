1e33ab53a5206e4f59efbae8b3e1fd168248c760
Antti Kekki <antti.kekki@reaktor.com>
2021-02-26 17:52:58 +0200

    Fix url coordinates multiple centering

--- src/index.ts
+++ src/index.ts
@@ -22,11 +22,21 @@ const determineStartLocation = () => {
   }
 }
 
+let prevCoordinatesFromURL: [number, number] | undefined
 const setMapLocationFromURLHash = () => {
   const coordinates = parseCoordinatesFromURL()
-  if (coordinates) {
+  /**
+   * Do not move map to URL coordines multiple times. URL updates when settings are changed
+   * so it would move map back to original target.
+   */
+  const sameAsPrevCoordinates =
+    prevCoordinatesFromURL && coordinates
+      ? prevCoordinatesFromURL.every((v, i) => v === coordinates[i])
+      : false
+  if (coordinates && !sameAsPrevCoordinates) {
     setMapLocation(coordinates)
     showSelectedLocationMarker(coordinates)
+    prevCoordinatesFromURL = coordinates
   }
 }
 
